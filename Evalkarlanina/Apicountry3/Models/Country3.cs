8b32b7fb692d3396dd42af9a0570e3b8b40f7e47
Karla Nicoll Nina Muñoz <a2016118253@estudiantes.upsa.edu.bo>
2021-03-19 21:54:19 -0400

    modelo incorporado

---
+++ Evalkarlanina/Apicountry3/Models/Country3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Apicountry3.Models
+{
+    public class Country3
+    {
+        [Key]
+        public string Name { get; set; }
+        [Required]
+        public string Capital { get; set; }
+        public int Population { get; set; }
+        public List<Double> Latlng { get; set; }
+        public List<String> Timezones { get; set; }
+        public List<Currency> Currencies { get; set; }
+        public string Flag { get; set; }
+
+    }
+}
